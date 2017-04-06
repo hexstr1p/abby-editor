043a93ef479237c4fcc2577e1969009403dfff46
panpanick <claud@openmailbox.org>
2017-04-06 18:32:25 -0400

    draws rows now

--- C/abby.c
+++ C/abby.c
@@ -5,8 +5,17 @@
 #include <termios.h>
 #include <unistd.h>
 
-struct termios org_term;
+// setting up ctrl-q to quit
+// use hexidecimal because C doesn't have binary literals
+// this mirrors what the ctrl key does and sets the upper 3 bits to 0
+#define CTRL_KEY(k) ((k) & 0x1f)
 
+
+
+
+/* Declarations */
+
+struct termios org_term;
 
 // disable console echo
 // this will make no input showup, like entering a password
@@ -19,6 +28,40 @@ void EndRawMode();
 // this kills the editor
 void Yamete(const char *s);
 
+// get keyboard input
+char ReadKey();
+void ProcessKey();
+
+// screen stuff
+void ScreenRefresh();
+void DrawRows();
+
+
+/* Functions */
+
+char ReadKey() {
+  int n;
+  char c;
+  while ((n = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (n == -1 && errno != EAGAIN) {
+      Yamete("read");
+    }
+  }
+  return c;
+}
+
+void ProcessKey() {
+  char c = ReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      // clear the screen on exit
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+  return;
+}
 
 void StartRawMode() {
   if (tcgetattr(STDIN_FILENO, &org_term) == -1) {
@@ -27,7 +70,6 @@ void StartRawMode() {
   atexit(EndRawMode);
 
   struct termios raw_term = org_term;
-
 
   // these are bitflags
   // first some old miscellaneous flags for old terminal emulators
@@ -63,9 +105,43 @@ void EndRawMode() {
 }
 
 void Yamete(const char *s) {
+  // clear the screen on exit
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
+  return;
 }
+
+void ScreenRefresh() {
+  // this is writing 4 bytes to the terminal
+  // the first is \x1b, the escape character
+  // then the other three bytes are [2J
+  // the stuff after the [ tells the terminal to do various things
+  // J clears the screen, and the 2 means the entire screen
+  // I'm using VT100 escape sequences here, they are supported most places
+  // later it may change to ncurses
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  // put the cursor at the top left
+  // H is cursor position
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  DrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  return;
+}
+
+void DrawRows() {
+  // just 24 rows for now
+  int i;
+  for (i = 0; i < 24; i++) {
+    write(STDOUT_FILENO, ">\r\n", 3);
+  }
+  return;
+}
+
+
 
 
 int main() {
@@ -74,14 +150,8 @@ int main() {
 
   // read every input as it comes in
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-      Yamete("read");
-    }
-    if (isprint(c)) printf("%d ('%c')\r\n", c, c);
-    else printf("%d\r\n", c);
-    if (c == 'q') break;
+    ScreenRefresh();
+    ProcessKey();
   }
-
   return 0;
 }
