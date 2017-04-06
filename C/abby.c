941ac6ba868cd88e4518d779fa8c843cba6793f5
panpanick <claud@openmailbox.org>
2017-04-06 12:45:16 -0400

    second benchmark reached

--- C/abby.c
+++ C/abby.c
@@ -1,5 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -15,27 +16,56 @@ void StartRawMode();
 // this will exit raw mode
 void EndRawMode();
 
+// this kills the editor
+void Yamete(const char *s);
+
 
 void StartRawMode() {
-  tcgetattr(STDIN_FILENO, &org_term);
+  if (tcgetattr(STDIN_FILENO, &org_term) == -1) {
+    Yamete("tcgetattr");
+  }
   atexit(EndRawMode);
 
   struct termios raw_term = org_term;
 
 
-  // this is a bitflag
+  // these are bitflags
+  // first some old miscellaneous flags for old terminal emulators
+  // BRKINT is break checking, INPCK is parity checking, ISTRIP is 8th bit strip
+  raw_term.c_iflag &= ~(BRKINT | INPCK | ISTRIP);
+  // CS8 is a bitmask
+  raw_term.c_cflag |= (CS8);
+  // the IXON flag stops manual software flow control from the old days
+  // I stops input flag, CR stops carriage return, and NL stops new line
+  raw_term.c_iflag &= ~(ICRNL | IXON);
+  // this one turns off output processing
+  raw_term.c_oflag &= ~(OPOST);
   // the ICANON lets it read input bit by bit instead of line by line
-  raw_term.c_lflag &= ~(ECHO | ICANON);
+  // the ISIG flag stops the ctrl-z and ctrl-y signals in the terminal
+  // the IEXTEN flag stops ctrl-o and ctrl-v
+  raw_term.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_term);
+  // I'm going to have read() timeout if it doesn't get input after a while
+  raw_term.c_cc[VMIN] = 0;
+  raw_term.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_term) == -1) {
+    Yamete("tcsetattr");
+  }
   return;
 }
 
 void EndRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_term);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_term) == -1) {
+    Yamete("tcsetattr");
+  }
   return;
 }
 
+void Yamete(const char *s) {
+  perror(s);
+  exit(1);
+}
 
 
 int main() {
@@ -43,11 +73,14 @@ int main() {
   StartRawMode();
 
   // read every input as it comes in
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-    // debugging purposes
-    if (isprint(c)) printf("%d ('%c')\n", c, c);
-    else printf("%d\n", c);
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+      Yamete("read");
+    }
+    if (isprint(c)) printf("%d ('%c')\r\n", c, c);
+    else printf("%d\r\n", c);
+    if (c == 'q') break;
   }
 
   return 0;
